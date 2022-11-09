a19fada8271e7a225907eeb2a843084a438b5862
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-11-09 10:40:20 +0530

    fix(bugs):fixed bugs

--- src/main/java/com/sims/services/StudentService.java
+++ src/main/java/com/sims/services/StudentService.java
@@ -11,33 +11,16 @@ import com.sims.services.interfaces.StudentInterface;
 
 public class StudentService implements StudentInterface {
 
+   
+   
    @Override
-   public boolean addStudent(Student student) throws Exception {
+   public boolean create(Student student) throws Exception {
       // TODO Auto-generated method stub
       return false;
    }
-
+   
    @Override
-   public boolean updateStudent(Student student) throws Exception {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public boolean deleteStudent(Integer id) throws Exception {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   @Override
-   public Student searchStudent(Integer id) throws Exception {
-      // TODO Auto-generated method stub
-      return null;
-   }
-
-   @Override
-   public ArrayList<Student> getAllStudents() throws Exception {
-
+   public ArrayList<Student> all() throws Exception {
       Connection con = ConnectionProvider.getConnection();
       PreparedStatement pstm = con.prepareStatement("SELECT * FROM Student");
 
@@ -47,14 +30,35 @@ public class StudentService implements StudentInterface {
 
       while (rst.next()) {
 
-         Student st = new Student(rst.getInt(1), rst.getString(2), rst.getString(3), rst.getString(4), rst.getString(5),
-               rst.getString(5), rst.getString(5));
+         Student st = new Student(rst.getInt(1),rst.getInt(2), rst.getString(3), rst.getString(4), rst.getString(5), rst.getString(6),
+               rst.getString(7), rst.getString(8),rst.getString(9));
 
          studentList.add(st);
 
       }
 
       return studentList;
+      
    }
+
+   @Override
+   public boolean update(Student student) throws Exception {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public boolean destroy(Integer id) throws Exception {
+      // TODO Auto-generated method stub
+      return false;
+   }
+
+   @Override
+   public Student search(Integer id) throws Exception {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+  
 
 }
